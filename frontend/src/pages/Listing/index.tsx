2fe60c53f34d7c6c9aa2d92fac9095028d527750
VictorPimentaTI <victorpimenta.ti@gmail.com>
2022-01-16 15:34:40 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -30,10 +30,14 @@ function Listing() {
       });
   }, [pageNumber]);
 
+  const handlePageChange = (newPageNumber : number) => {
+    setPageNumber(newPageNumber);
+  }
+
   return(
     <>
       {/* <p>{pageNumber}</p> */}
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
 
       <div className="container">
         <div className="row">
